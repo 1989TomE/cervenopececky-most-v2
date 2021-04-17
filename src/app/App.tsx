225c7dc02ea68be1ba3cf45947b8b4b31cdf8d07
Tomas Ebert <Tomas.Ebert@adastragrp.com>
2021-04-17 16:36:27 +0200

    Added document head

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import DocumentHead from "./components/DocumentHead";
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <React.StrictMode>
+      <DocumentHead />
+      <h1>Test</h1>
+    </React.StrictMode>
+  );
 };
 
 export default App;
---
+++ src/app/components/DocumentHead.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+
+const DocumentHead = () => {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Červenopečecký mošt</title>
+    </Helmet>
+  );
+};
+
+export default DocumentHead;
