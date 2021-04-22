94a75e8678c88ef3d53e06819a40cda82ff04357
Tomas Ebert <Tomas.Ebert@adastragrp.com>
2021-04-22 21:33:20 +0200

    Added page animation

--- src/components/PagesContent/Layout/Page.tsx
+++ src/components/PagesContent/Layout/Page.tsx
@@ -3,15 +3,29 @@ import homeBackgroundImage from "@root/assets/webBackground.jpg";
 import PageTopPart from "@src/components/PagesContent/TopPart/PageTopPart";
 import Navigation from "@src/components/Navigation/MainNavigation";
 import { pageMinHeight } from "@src/styles/page";
-import darkLogo from "@root/assets/logo_dark.jpg";
+import { useState, useEffect } from "react";
+// import { CSSTransition } from "react-transition-group";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 const PageWrapper = ({ children }: Props) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      console.log("mounting");
+      setMounted(true);
+    });
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
   return (
-    <Wrapper>
+    <Wrapper mounted={mounted} className="pageWrapper">
       <div className="pageContainer">
         <PageTopPart />
         <Navigation />
@@ -31,7 +45,7 @@ const PageWrapper = ({ children }: Props) => {
 
 export default PageWrapper;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<{ mounted: boolean }>`
   height: ${pageMinHeight}px;
   max-width: 2560px;
   background-image: url(${homeBackgroundImage});
@@ -42,6 +56,11 @@ export const Wrapper = styled.div`
   .pageContainer {
     margin: 0 auto;
     max-width: 800px;
+
+    .contentContainer {
+      opacity: ${(props) => (props.mounted ? 1 : 0)};
+      transition: opacity 0.5s ease-in-out;
+    }
 
     .footer {
       display: flex;
