e27dea2db913ad88f98f77ceb39e9e2ff51aa0f8
Tomas Ebert <Tomas.Ebert@adastragrp.com>
2021-04-25 19:26:47 +0200

    Routes lazy loaded

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,34 +1,49 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Page } from "./index";
 import NotFound from "./NotFound/Notfound";
-import Home from "@src/pages/Home/Home";
-import WorkPlace from "@src/pages/WorkPlace/WorkPlace";
-import Making from "@src/pages/Making/Making";
-import PublicAppleJuicing from "@src/pages/PublicAppleJuicing/PublicAppleJuicing";
-import FruitOrchards from "./FruitOrchards/FruitOrchards";
-import Contact from "./Contact/Contact";
-import WantToFruitPress from "./WantToFruitPress/WantToFruitPress";
-import OurProducts from "./OurProducts/OurProducts";
-import EShop from "./WhereToBuy/WhereToBuy";
+
+const Home = React.lazy(() => import("@src/pages/Home/Home"));
+const WorkPlace = React.lazy(() => import("@src/pages/WorkPlace/WorkPlace"));
+const PublicAppleJuicing = React.lazy(
+  () => import("@src/pages/PublicAppleJuicing/PublicAppleJuicing")
+);
+const Making = React.lazy(() => import("@src/pages/Making/Making"));
+const FruitOrchards = React.lazy(
+  () => import("@src/pages/FruitOrchards/FruitOrchards")
+);
+const Contact = React.lazy(() => import("@src/pages/Contact/Contact"));
+const WantToFruitPress = React.lazy(
+  () => import("@src/pages/WantToFruitPress/WantToFruitPress")
+);
+const OurProducts = React.lazy(
+  () => import("@src/pages/OurProducts/OurProducts")
+);
+const WhereToBuy = React.lazy(() => import("@src/pages/WhereToBuy/WhereToBuy"));
 
 const Routes = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path={Page.Home} component={Home} />
+      <React.Suspense fallback={null}>
+        <Switch>
+          <Route exact path={Page.Home} component={Home} />
 
-        <Route path={Page.WorkPlace} component={WorkPlace} />
-        <Route path={Page.PublicAppleJuicing} component={PublicAppleJuicing} />
-        <Route path={Page.Making} component={Making} />
-        <Route path={Page.FruitOrchards} component={FruitOrchards} />
+          <Route path={Page.WorkPlace} component={WorkPlace} />
+          <Route
+            path={Page.PublicAppleJuicing}
+            component={PublicAppleJuicing}
+          />
+          <Route path={Page.Making} component={Making} />
+          <Route path={Page.FruitOrchards} component={FruitOrchards} />
 
-        <Route path={Page.WhereToBuy} component={EShop} />
-        <Route path={Page.OurProducts} component={OurProducts} />
-        <Route path={Page.Contact} component={Contact} />
-        <Route path={Page.WantToFruitPress} component={WantToFruitPress} />
+          <Route path={Page.WhereToBuy} component={WhereToBuy} />
+          <Route path={Page.OurProducts} component={OurProducts} />
+          <Route path={Page.Contact} component={Contact} />
+          <Route path={Page.WantToFruitPress} component={WantToFruitPress} />
 
-        <Route component={NotFound} />
-      </Switch>
+          <Route component={NotFound} />
+        </Switch>
+      </React.Suspense>
     </Router>
   );
 };
